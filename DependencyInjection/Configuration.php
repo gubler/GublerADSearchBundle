ed9c5365a27f2cd759a0417504c2658a72c3832c
Daryl Gubler <daryl@dev88.co>
2019-03-28 11:50:42 -0400

    Update to support Symfony 4.2

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -24,8 +24,13 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
 
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('gubler_ad_search');
+        $treeBuilder = new TreeBuilder('gubler_ad_search');
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('gubler_ad_search');
+        }
 
         $rootNode
             ->children()
